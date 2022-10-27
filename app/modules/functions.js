b376d6d8570f0c6424fbe02e4862dde6ffcaae34
mohammad82a <mohammadalii82ytposd@gmail.com>
2022-10-27 20:00:23 +0330

    create route of user profile

--- app/modules/functions.js
+++ app/modules/functions.js
@@ -11,7 +11,14 @@ function tokenGenerator(payload){
     return token
 }
 
+function verifyJwtToken(token){
+   const result = jwt.verify(token,process.env.SECERET_KEY)
+   if(!result?.username) throw {status : 401, message : "لطفا وارد حساب کاربری خود شوید"}
+   return result
+}
+
 module.exports = {
     hashString,
-    tokenGenerator
+    tokenGenerator,
+    verifyJwtToken
 }
