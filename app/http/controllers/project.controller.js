c8c018140ea46e06442909c693444aab2f0e2a19
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2022-11-01 23:40:07 +0330

    upload image with express-fileUpload

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -3,9 +3,9 @@ const { ProjectModel } = require("../../models/project");
 class ProjectController{
    async createProject(req,res,next){
             try {
-                const {text , title} = req.body;
+                const {text , title , image} = req.body;
                 const owner = req.user._id;
-                const result = await ProjectModel.create({title,text,owner})
+                const result = await ProjectModel.create({title,text,owner,image})
                 if(!result) throw {status : 400 , message : "ایجاد پروژه با مشکل مواجه شد"}
                 return res.status(201).json({
                     status : 201,
--- app/router/project.js
+++ app/router/project.js
@@ -2,10 +2,12 @@ const { ProjectController } = require("../http/controllers/project.controller");
 const { checkLogin } = require("../http/middlewares/autoLogin");
 const { expressValidatorMapper } = require("../http/middlewares/checkerrors");
 const { createProjectValidator } = require("../http/validations/project");
+const expressFileUpload = require("express-fileupload");
+const { uploadFile } = require("../modules/express-fileupload");
 
 const router = require("express").Router();
 
-router.post("/create" , checkLogin, createProjectValidator(), expressValidatorMapper , ProjectController.createProject)
+router.post("/create" , expressFileUpload(), checkLogin, uploadFile, createProjectValidator(), expressValidatorMapper , ProjectController.createProject)
 
 module.exports = {
     projectRoutes : router
